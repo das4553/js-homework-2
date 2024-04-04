c828accd3699ad0901950a44c1cb78d43f4f7080
Gioo1111 <135989805+das4553@users.noreply.github.com>
2024-04-04 14:19:43 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const  myArray = [1,2,3,4,5]
+const  arrayResult = (myArray[0] + myArray[1] + myArray[2] + myArray[3] + myArray[4]) /5;
+console.log(arrayResult)
+
+
+const  myArray2 =[
+    {
+        name: "apple",
+        age: undefined
+    },
+    {
+        name:"Tome cruise",
+        age:61,
+    },
+]
+myArray2.push({name:"Annabelle Wallis", age:39})
+
+
+console.log(myArray2)
+
+
+
+
+const  myObject = {
+    firstName: 'Giorgi',
+    lastName:"Mariamuli",
+    address:["Khosharauli"],
+    age:18,
+    phoneNumbers:[599276577],
+}
+
+console.log(`My name is ${myObject["firstName"]}\nMy age is ${myObject["age"]}\nMy address is ${myObject["address"]}`)
+
